2e7ba2d5cadcd571a9048e7510a6007844326d45
DiabolicaTrix <alexy.mantha.1@ulaval.ca>
2021-01-17 11:03:14 -0500

    Initial project setup

---
+++ src/main/java/dev/dtrix/voicechat/VoiceChat.java
@@ -0,0 +1,11 @@
+package dev.dtrix.voicechat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(name = VoiceChat.NAME, modid = VoiceChat.MODID)
+public class VoiceChat {
+
+    public static final String NAME = "DTrix's Voice Chat";
+    public static final String MODID = "dtrix-voicechat";
+
+}
